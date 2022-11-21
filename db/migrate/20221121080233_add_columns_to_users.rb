538312fe956d55c96b7bc7873baedce7ec33945c
Fred <72956693+twk-mn@users.noreply.github.com>
2022-11-21 18:24:56 +0900

    Update 20221121080233_add_columns_to_users.rb
    Changed admin column from integer to boolean (default: false).

--- db/migrate/20221121080233_add_columns_to_users.rb
+++ db/migrate/20221121080233_add_columns_to_users.rb
@@ -2,6 +2,6 @@ class AddColumnsToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :name, :string
     add_column :users, :phone, :integer
-    add_column :users, :admin, :integer
+    add_column :users, :admin, :boolean, default: false
   end
 end
