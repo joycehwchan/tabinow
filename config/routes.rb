97b2c76cbbed8095b04ac76fd843d5e2a3f8763d
Christopher Ikaika Crespo <topherikaika@gmail.com>
2022-11-21 16:32:51 +0900

    create-routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :itineraries, only: [:index, :show, :create, :update] do
+    member do
+      get :draft
+      get :pay
+      post :send_confirmation
+      post :send_draft
+    end
+  end
+  resources :items, only: :destroy
+  resources :days, only: :destroy do
+    resources :items, only: :create
+  end
 end
