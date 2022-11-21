d6f81fba97fffd20627c1f24800526c17b954b91
Hafid Qarchi <haqarchi@gmail.com>
2022-11-21 23:11:32 +0900

    initial validation

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,2 +1,4 @@
 class Itinerary < ApplicationRecord
+  belongs_to :employee, class_name: "User"
+  belongs_to :client, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :itineraries_as_employee, class_name: "Itinerary", foreign_key: :employee_id
+  has_many :itineraries_as_client, class_name: "Itinerary", foreign_key: :client_id
 end
