9bd9d1d6f2ac3f1e9d648df10eb60e33aa6d4852
Fred <72956693+twk-mn@users.noreply.github.com>
2022-11-21 18:25:53 +0900

    Update 20221121080144_create_addresses.rb
    Small fixes to be more precise (and match seeds).

--- db/migrate/20221121080144_create_addresses.rb
+++ db/migrate/20221121080144_create_addresses.rb
@@ -1,8 +1,9 @@
 class CreateAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :addresses do |t|
-      t.string :zipcode
+      t.string :zip_code
       t.string :street
+      t.string :street_two
       t.string :city
       t.string :country
       t.references :user, null: false, foreign_key: true
