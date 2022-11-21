3700b1fef4ad5529cbe35526bc7b0c5ecf4092cf
Joyce Chan <joycehwchan@gmail.com>
2022-11-22 00:24:56 +0900

    style devise forms and sidebar

---
+++ app/javascript/controllers/sidebar_controller.js
@@ -0,0 +1,38 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="sidebar"
+export default class extends Controller {
+  static targets = ["content", "button"];
+  connect() {}
+
+  toggle(e) {
+    const openIcon = `<i class="fa-solid fa-chevron-left"></i>`;
+    const closeIcon = `<i class="fa-solid fa-chevron-right"></i>`;
+    this.element.classList.toggle("sidebar-hide");
+    this.contentTarget.classList.toggle("d-none");
+    this.buttonTarget.classList.toggle("shift-btn");
+    this.buttonTarget.classList.toggle("btn-light");
+    this.buttonTarget.classList.toggle("btn-primary");
+    if (this.buttonTarget.innerHTML != openIcon) {
+      this.buttonTarget.innerHTML = closeIcon;
+    } else {
+      this.buttonTarget.innerHTML = openIcon;
+    }
+  }
+
+  collaspe(e) {
+    if (window.innerWidth <= 992) {
+      this.element.classList.add("sidebar-hide");
+      this.contentTarget.classList.add("d-none");
+      this.buttonTarget.classList.add("shift-btn");
+      this.buttonTarget.classList.remove("btn-light");
+      this.buttonTarget.classList.add("btn-primary");
+    } else {
+      this.element.classList.remove("sidebar-hide");
+      this.contentTarget.classList.remove("d-none");
+      this.buttonTarget.classList.remove("shift-btn");
+      this.buttonTarget.classList.add("btn-light");
+      this.buttonTarget.classList.remove("btn-primary");
+    }
+  }
+}
