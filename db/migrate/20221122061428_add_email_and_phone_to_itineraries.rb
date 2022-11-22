3e517ea6c0d06be0747ef79e41049539d28c86da
Fred Faeger <fred@tawakemono.com>
2022-11-22 16:17:03 +0900

    Fixed the seeds, migration and validation

--- db/migrate/20221122061428_add_email_and_phone_to_itineraries.rb
+++
@@ -1,6 +0,0 @@
-class AddEmailAndPhoneToItineraries < ActiveRecord::Migration[7.0]
-  def change
-    add_column :itineraries, :email, :string
-    add_column :itineraries, :phone, :string
-  end
-end
