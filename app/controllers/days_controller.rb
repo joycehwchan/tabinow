60340822e65c3a1fcaa73018f13b301b58839393
Christopher Ikaika Crespo <topherikaika@gmail.com>
2022-11-22 15:17:36 +0900

    create method for days controller

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,2 +1,23 @@
 class DaysController < ApplicationController
+
+  def new
+    @day = Day.new
+  end
+
+  def create
+    @day = Day.new(day_params)
+    @day.itinerary = params[:id]
+    if day.save
+      flash[:success] = "Day in itinerary created!"
+      redirect_to itinerary_path(@day.itinerary)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def day_params
+    params.require(:day).permit(:number)
+  end
 end
