5a2101478a28a30fbd4bbdc5f0860f4708dabea3
Fred Faeger <fred@tawakemono.com>
2022-11-22 12:06:10 +0900

    Don't merge, problem with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@
 #
 # Examples:
 #
-#   movies = Movie.save([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.save(name: "Luke", movie: movies.first)
+#   movies = Movie.save!([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.save!(name: "Luke", movie: movies.first)
 
 # Seed for clients
 5.times do
@@ -12,8 +12,9 @@
   client = User.new(name: Faker::FunnyName.two_word_name,
                     email: Faker::Internet.safe_email,
                     password: "Password123",
-                    phone: Faker::PhoneNumber.cell_phone_in_e164)
-  client.save
+                    phone: Faker::PhoneNumber.cell_phone_in_e164,
+                    admin: false)
+  client.save!
 
   # Adding address to client:
   address = Address.new(street: Faker::Address.street_address,
@@ -22,7 +23,7 @@
                         city: Faker::Address.city,
                         country: Faker::Address.country,
                         user_id: client)
-  address.save
+  address.save!
 end
 # Creating Employee user:
 client = User.new(name: "TabiNowEmp",
@@ -30,7 +31,7 @@ client = User.new(name: "TabiNowEmp",
                   password: "Password123",
                   admin: true,
                   phone: Faker::PhoneNumber.cell_phone_in_e164)
-client.save
+client.save!
 
 
 # Seed for itinerary
@@ -44,13 +45,17 @@ client.save
   # Randomly generates a number of days
   days_number = rand(1..15)
   # Createing itinerary
-  itinerary = Itinerary.save(name: "#{days_number} in #{location}",
-                                location: location,
-                                status: rand(0..3),
-                                client_id: client,
-                                employee_id: employee)
+  itinerary = Itinerary.new(name: "#{days_number} in #{location}",
+                            location: location,
+                            status: rand(0..3),
+                            client_id: client,
+                            employee_id: employee)
+
+  itinerary.save!
+
   # Creating a Day db
-  Day.save(days: days_number, itinerary_id: itinerary)
+  days_number = Day.new(number: days_number, itinerary_id: itinerary)
+  days_number.create
 
   # Generate stay, restuarants, activities
   days_number.times do |day|
@@ -58,64 +63,64 @@ client.save
     stay_name = Faker::Games::SuperMario.character
     stay_type = ["Hotel", "Hostel", "Ryokan", "Camping Ground"].sample
     stay = Content.new(name: "#{stay_name}'s #{stay_type}",
-                        price: rand(15_000..100_000),
-                        location: location,
-                        category: "stay",
-                        rating: rand(1..5),
-                        description: Faker::Lorem.paragraph(sentence_count: 2),
-                        api_id: rand(1..5),
-                        day_id: day,
-                        status: rand(0..3))
-    stay.save
+                       price: rand(15_000..100_000),
+                       location: location,
+                       category: "stay",
+                       rating: rand(1..5),
+                       description: Faker::Lorem.paragraph(sentence_count: 2),
+                       api_id: rand(1..5),
+                       day_id: day,
+                       status: rand(0..3))
+    stay.save!
 
     # lunch_restaurant_name = Faker::JapaneseMedia::StudioGhibli.character
     # Generate restuarant for lunch
     lunch = Content.new(name: "#{Faker::Restaurant.name} (#{Faker::Restaurant.type})",
-                         price: rand(1000..15_000),
-                         location: location,
-                         category: "lunch",
-                         rating: rand(1..5),
-                         description: Faker::Restaurant.description,
-                         api_id: rand(1..5),
-                         day_id: day,
-                         status: rand(0..3))
-    lunch.save
+                        price: rand(1000..15_000),
+                        location: location,
+                        category: "lunch",
+                        rating: rand(1..5),
+                        description: Faker::Restaurant.description,
+                        api_id: rand(1..5),
+                        day_id: day,
+                        status: rand(0..3))
+    lunch.save!
 
     # Generate restuarant for dinner
     dinner = Content.new(name: "#{Faker::Restaurant.name} (#{Faker::Restaurant.type})",
-                          price: rand(5000..55_000),
-                          location: location,
-                          category: "dinner",
-                          rating: rand(1..5),
-                          description: Faker::Lorem.paragraph(sentence_count: 2),
-                          api_id: rand(1..5),
-                          day_id: day,
-                          status: rand(0..3))
-    dinner.save
+                         price: rand(5000..55_000),
+                         location: location,
+                         category: "dinner",
+                         rating: rand(1..5),
+                         description: Faker::Lorem.paragraph(sentence_count: 2),
+                         api_id: rand(1..5),
+                         day_id: day,
+                         status: rand(0..3))
+    dinner.save!
 
     # Generate morning activity
     morning_activity = Content.new(name: "#{Faker::Hobby.activity} with #{Faker::JapaneseMedia::StudioGhibli.character}",
-                                    price: rand(1000..15_000),
-                                    location: location,
-                                    category: "morning_activity",
-                                    rating: rand(1..5),
-                                    description: Faker::Lorem.paragraph(sentence_count: 2),
-                                    api_id: rand(1..5),
-                                    day_id: day,
-                                    status: rand(0..3))
-  
-    morning_activity.save
-    
+                                   price: rand(1000..15_000),
+                                   location: location,
+                                   category: "morning_activity",
+                                   rating: rand(1..5),
+                                   description: Faker::Lorem.paragraph(sentence_count: 2),
+                                   api_id: rand(1..5),
+                                   day_id: day,
+                                   status: rand(0..3))
+
+    morning_activity.save!
+
     # Generate afternoon activity
     afternoon_activity = Content.new(name: "#{Faker::Sports.unusual_sport} at #{Faker::Movies::StarWars.planet}",
-                                      price: rand(4000..25_000),
-                                      location: location,
-                                      category: "afternoon_activity",
-                                      rating: rand(1..5),
-                                      description: Faker::Lorem.paragraph(sentence_count: 2),
-                                      api_id: rand(1..5),
-                                      day_id: day,
-                                      status: rand(0..3))
-    afternoon_activity.save
+                                     price: rand(4000..25_000),
+                                     location: location,
+                                     category: "afternoon_activity",
+                                     rating: rand(1..5),
+                                     description: Faker::Lorem.paragraph(sentence_count: 2),
+                                     api_id: rand(1..5),
+                                     day_id: day,
+                                     status: rand(0..3))
+    afternoon_activity.save!
   end
 end
