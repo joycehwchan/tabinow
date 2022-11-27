4101275f7d3a0703be37e0d6d581f36c90797321
Joyce Chan <joycehwchan@gmail.com>
2022-11-27 21:50:51 +0900

    added typewriter

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,16 +2,16 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "./application";
 
-import FormController from "./form_controller"
-application.register("form", FormController)
+import FormController from "./form_controller";
+application.register("form", FormController);
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import HelloController from "./hello_controller";
+application.register("hello", HelloController);
 
-import SidebarController from "./sidebar_controller"
-application.register("sidebar", SidebarController)
+import SidebarController from "./sidebar_controller";
+application.register("sidebar", SidebarController);
 
-import SortableController from "./sortable_controller"
-application.register("sortable", SortableController)
+import SortableController from "./sortable_controller";
+application.register("sortable", SortableController);
