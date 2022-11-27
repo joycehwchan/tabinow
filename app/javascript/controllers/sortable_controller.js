81b72cd5421c94f1d4eb746bbcb61146269e3e51
Hafid Qarchi <haqarchi@gmail.com>
2022-11-27 15:35:21 +0900

    added the sortable data controller

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -1,7 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
-
+import Sortable from "sortablejs"
 // Connects to data-controller="sortable"
 export default class extends Controller {
   connect() {
+    console.log("hi from sortable");
   }
 }
