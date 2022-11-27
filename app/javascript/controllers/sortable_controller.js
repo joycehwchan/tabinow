fff1a5019e2abe1d2f34d22a2b9e197741188aad
Hafid Qarchi <haqarchi@gmail.com>
2022-11-27 15:45:59 +0900

    sortable js fuctionality

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -3,6 +3,13 @@ import Sortable from "sortablejs"
 // Connects to data-controller="sortable"
 export default class extends Controller {
   connect() {
-    console.log("hi from sortable");
+    // console.log("hi from sortable");
+    Sortable.create(this.element, {
+      ghostClass: "ghost",
+      animation: 150,
+      onEnd: (event) => {
+        alert(`${event.oldIndex} moved to ${event.newIndex}`)
+      }
+    })
   }
-}
+  }
