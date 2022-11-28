2bccce5a7168f4c55104418eb86eadc0beb43496
Joyce Chan <joycehwchan@gmail.com>
2022-11-28 15:05:43 +0900

    combine filter and search stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,22 +2,19 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "./application";
 
-import FilterController from "./filter_controller"
-application.register("filter", FilterController)
+import FormController from "./form_controller";
+application.register("form", FormController);
 
-import FormController from "./form_controller"
-application.register("form", FormController)
+import HelloController from "./hello_controller";
+application.register("hello", HelloController);
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import SearchController from "./search_controller";
+application.register("search", SearchController);
 
-import SearchController from "./search_controller"
-application.register("search", SearchController)
+import SidebarController from "./sidebar_controller";
+application.register("sidebar", SidebarController);
 
-import SidebarController from "./sidebar_controller"
-application.register("sidebar", SidebarController)
-
-import SortableController from "./sortable_controller"
-application.register("sortable", SortableController)
+import SortableController from "./sortable_controller";
+application.register("sortable", SortableController);
