ec284fbc0865680816c2275356ad2a9ba0ad3ab8
Hafid Qarchi <haqarchi@gmail.com>
2022-11-29 17:04:22 +0900

    logic for updating column added

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -2,14 +2,41 @@ import { Controller } from "@hotwired/stimulus";
 import Sortable from "sortablejs";
 // Connects to data-controller="sortable"
 export default class extends Controller {
+  static values = {
+    itinerary: String,
+    day: String,
+  };
+
   connect() {
     // console.log("hi from sortable");
     Sortable.create(this.element, {
       // ghostClass: "ghost",
       animation: 150,
-      // onEnd: (event) => {
-      //   alert(`${event.oldIndex} moved to ${event.newIndex}`)
-      // }
+      onEnd: (event) => {
+        // alert(`${event.oldIndex} moved to ${event.newIndex}`);
+        const id = event.item.dataset.id;
+        const url = `/contents/${id}`;
+        const csrfToken = document.querySelector("[name='csrf-token']").content;
+        const cardsContainer = document.querySelector("#list");
+
+        const data = JSON.stringify({
+          position: event.newIndex + 1,
+          itinerary_id: this.itineraryValue,
+          day: this.dayValue,
+        });
+
+        const options = {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            "X-CSRF-Token": csrfToken,
+          },
+          body: data,
+        };
+        fetch(url, options)
+          .then((response) => response.text())
+          .then((data) => console.log(data));
+      },
     });
   }
 }
