42dcbcd07ae931d7f15ae192bf394ff6d8100e64
Hafid Qarchi <haqarchi@gmail.com>
2022-11-29 17:12:17 +0900

    contents controller created

---
+++ app/controllers/contents_controller.rb
@@ -0,0 +1,24 @@
+class ContentsController < ApplicationController
+  def update
+    p params
+    content = Content.find(params[:id])
+    itinerary = Itinerary.find(params[:itinerary_id].to_i)
+    content.update(contents_params)
+    day = itinerary.days[params[:day].to_i - 1]
+
+    # head :ok
+
+    # binding.break
+    authorize content
+    respond_to do |format|
+      format.html { redirect_to itinerary_path(itinerary), status: :see_other }
+      format.text { render partial: "itineraries/content", locals: { contents: day.contents }, formats: [:html] }
+    end
+  end
+
+  private
+
+  def contents_params
+    params.require(:content).permit(:position)
+  end
+end
