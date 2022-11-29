96aabde86036ca0b1662659832e5b638347ea88f
Hafid Qarchi <haqarchi@gmail.com>
2022-11-29 17:13:32 +0900

    contents policy defined

---
+++ app/policies/content_policy.rb
@@ -0,0 +1,12 @@
+class ContentPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+
+  def update?
+    true
+  end
+end
