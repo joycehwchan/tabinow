9e2ed50bcc3bca626d3926e460d1c1718435c702
Hafid Qarchi <haqarchi@gmail.com>
2022-11-30 10:02:50 +0900

    refactor the js code

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -8,43 +8,39 @@ export default class extends Controller {
   };
 
   connect() {
-    const cardsContainer = document.querySelector("#list");
     // console.log("hi from sortable");
     Sortable.create(this.element, {
       // ghostClass: "ghost",
       animation: 150,
-      onEnd: (event) => {
-        // alert(`${event.oldIndex} moved to ${event.newIndex}`);
-        const url = `/itineraries/${this.itineraryValue}/move`;
-        const csrfToken = document.querySelector("[name='csrf-token']").content;
-        const list = document.querySelectorAll("[data-id]");
-
-        console.log(cardsContainer);
-        const listItems = Array.from(list).map((item, index) => ({
-          content: item.dataset.id,
-          // url: `/contents/${item.dataset.id}`,
-          currentIndex: index + 1,
-        }));
-        const data = JSON.stringify({
-          // position: event.newIndex + 1,
-          list: listItems,
-          // itinerary_id: this.itineraryValue,
-          day: this.dayValue,
-        });
-
-        const options = {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-            "X-CSRF-Token": csrfToken,
-            Accept: "text/plain",
-          },
-          body: data,
-        };
-        fetch(url, options)
-          .then((response) => response.text())
-          .then((data) => (cardsContainer.innerHTML = data));
-      },
+      onEnd: this.end.bind(this),
     });
+  }
+
+  end(event) {
+    const cardsContainer = document.querySelector("#list");
+    const url = `/itineraries/${this.itineraryValue}/move`;
+    const csrfToken = document.querySelector("[name='csrf-token']").content;
+    const list = document.querySelectorAll("[data-id]");
+
+    const listItems = Array.from(list).map((item, index) => ({
+      content: item.dataset.id,
+      currentIndex: index + 1,
+    }));
+    const data = JSON.stringify({
+      list: listItems,
+      day: this.dayValue,
+    });
+    const options = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": csrfToken,
+        Accept: "text/plain",
+      },
+      body: data,
+    };
+    fetch(url, options)
+      .then((response) => response.text())
+      .then((data) => (cardsContainer.innerHTML = data));
   }
 }
