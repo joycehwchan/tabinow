3c3acf905a73f65fc1500d4087596bdcecc73fd0
Fred Faeger <fred@tawakemono.com>
2022-11-30 12:37:20 +0900

    Removed the migration that was not working. Added Admin: true to test users. Limited the max days in seeds to 3 instead of 5

--- db/migrate/20221130030245_remove_position_from_categories.rb
+++
@@ -1,5 +0,0 @@
-class RemovePositionFromCategories < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :categories, :position
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -42,18 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_030354) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "addresses", force: :cascade do |t|
-    t.string "zip_code"
-    t.string "street"
-    t.string "street_two"
-    t.string "city"
-    t.string "country"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_addresses_on_user_id"
-  end
-
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.string "sub_category"
@@ -139,6 +127,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_030354) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "phone"
+    t.string "zipcode"
+    t.string "street"
+    t.string "city"
+    t.string "country"
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -146,7 +138,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_030354) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "addresses", "users"
   add_foreign_key "categories", "days"
   add_foreign_key "days", "itineraries"
   add_foreign_key "itineraries", "users", column: "client_id"
