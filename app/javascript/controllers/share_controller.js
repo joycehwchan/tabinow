2e56b244c1c5bdbe376147af6fc3f753bbcdf8b5
Hafid Qarchi <haqarchi@gmail.com>
2022-12-01 16:42:50 +0900

    pop up for preview

--- app/javascript/controllers/share_controller.js
+++ app/javascript/controllers/share_controller.js
@@ -6,14 +6,14 @@ export default class extends Controller {
   };
   static targets = ["result", "button", "icon"];
   connect() {
-    console.log(this.urlValue);
-    if (navigator.share) {
-      this.buttonTarget.innerText = "Share";
-    } else {
-      this.iconTarget.classList.remove("fa-share-nodes");
-      this.iconTarget.classList.add("fa-clipboard");
-      this.buttonTarget.innerText = "Copy link";
-    }
+    // console.log(this.urlValue);
+    // if (navigator.share) {
+    //   this.buttonTarget.innerText = "Share";
+    // } else {
+    //   this.iconTarget.classList.remove("fa-share-nodes");
+    //   this.iconTarget.classList.add("fa-clipboard");
+    //   this.buttonTarget.innerText = "Copy link";
+    // }
   }
   async share(e) {
     e.preventDefault();
@@ -22,15 +22,15 @@ export default class extends Controller {
       text: "my trip",
       title: this.titleValue,
     };
-    if (navigator.share) {
-      try {
-        await navigator.share(shareData);
-        this.resultTarget.textContent = "MDN Shared successfully";
-      } catch (err) {
-        this.resultTarget.textContent = `${err}`;
-      }
-    } else {
-      navigator.clipboard.writeText(this.urlValue);
+    // if (navigator.share) {
+    try {
+      await navigator.share(shareData);
+      this.resultTarget.textContent = "MDN Shared successfully";
+    } catch (err) {
+      this.resultTarget.textContent = `${err}`;
     }
+    // } else {
+    //   navigator.clipboard.writeText(this.urlValue);
+    // }
   }
 }
