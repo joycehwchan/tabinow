c0e54fa95094140aa74f44237071e96908de8fc0
Hafid Qarchi <haqarchi@gmail.com>
2022-12-01 12:32:21 +0900

    added target with stimilus

--- app/javascript/controllers/share_controller.js
+++ app/javascript/controllers/share_controller.js
@@ -1,7 +1,12 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="share"
 export default class extends Controller {
+  static values = {
+    url: String,
+  };
+  static targets = ["results"];
   connect() {
+    console.log("hi");
   }
 }
