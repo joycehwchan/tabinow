cf4848abc0ea2b4bd40dc3f28b290f4c76f024fc
Hafid Qarchi <haqarchi@gmail.com>
2022-12-01 18:28:46 +0900

    removed an required code and copy link button text change after 2 sec

--- app/javascript/controllers/share_controller.js
+++ app/javascript/controllers/share_controller.js
@@ -2,35 +2,17 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static values = {
     url: String,
-    title: String,
   };
-  static targets = ["result", "button", "icon"];
-  connect() {
-    console.log(this.urlValue);
-    if (navigator.share) {
-      this.buttonTarget.innerText = "Share";
-    } else {
-      this.iconTarget.classList.remove("fa-share-nodes");
-      this.iconTarget.classList.add("fa-clipboard");
-      this.buttonTarget.innerText = "Copy link";
-    }
-  }
-  async share(e) {
+  static targets = ["button"];
+  connect() {}
+  share(e) {
     e.preventDefault();
-    const shareData = {
-      url: this.urlValue,
-      text: "my trip",
-      title: this.titleValue,
-    };
-    if (navigator.share) {
-      try {
-        await navigator.share(shareData);
-        this.resultTarget.textContent = "MDN Shared successfully";
-      } catch (err) {
-        this.resultTarget.textContent = `${err}`;
-      }
-    } else {
-      navigator.clipboard.writeText(this.urlValue);
-    }
+    navigator.clipboard.writeText(this.urlValue);
+    this.buttonTarget.innerText = "Copied";
+
+    setTimeout(() => this.changeText(), 2000);
+  }
+  changeText() {
+    this.buttonTarget.innerText = "Copy link";
   }
 }
